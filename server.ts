4e174c69089e0c1d283c5c9f6c3e18f49fb6734e
Ratan-Sen <ratansen.iitg@gmail.com>
2024-07-04 17:26:05 +0530

    cleaning before deployment

--- server.ts
+++ server.ts
@@ -48,10 +48,6 @@ io.on('connection', (socket) => {
     
 });
 
-
-
-const PORT = 3000;
-server.listen(PORT, () => {
-    console.log(`listening on *:${PORT}`);
-});
+const PORT = process.env.PORT || 3000;
+server.listen(PORT);
 
